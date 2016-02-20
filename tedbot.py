fc7998d3ab08cca17f436f63dbc1482b2ba075da
Matt Boggie <mboggie@mac.com>
2016-02-20 00:54:32 -0500

    grabs images for slides, and formats as JSON

--- tedbot.py
+++ tedbot.py
@@ -4,35 +4,72 @@ import sys
 import math
 import random
 import re
+import flickrapi
+import yaml
+import json
+from pattern.en import parse
+from pattern.en import tag
 
 GRAF_LENGTH = 8
 GRAF_RANGE = 3
 DEBUG = True
 
+with open("config.yml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+    flickr_key = cfg['flickr']['api_key']
+    flickr_secret = cfg['flickr']['api_key']
+
 # generate a target length for a paragraph, of the range LENGTH +/- [0-RANGE]
 def initTarget():
 	return GRAF_LENGTH + int(math.floor(random.triangular(0-GRAF_RANGE, GRAF_RANGE)))
 
-#given a list of sentences, create a long string broken up into paragraphs
+def getImage(refstring):
+	tagged = tag(refstring)
+
+	nouns = [word for word,pos in tagged if pos == 'NNP' or pos == 'NP' or pos == 'NN']
+	try:
+		query = random.choice(nouns)
+	except IndexError:
+		#somehow this string has no nouns!
+		if DEBUG: print("Paragraph with no nouns:\n" + refstring, file=sys.stderr)
+		return None
+	
+	if DEBUG: print(query, file=sys.stderr)
+
+	flickr = flickrapi.FlickrAPI(flickr_key, flickr_secret, format='parsed-json')
+	result = flickr.photos_search(api_key = flickr_key, text = query, privacy_filter = 1, safe_search=1, sort='interestingness-desc', orientation="landscape")
+	try:
+		pick = random.choice(result['photos']['photo'])
+		url = 'https://farm' + str(pick['farm']) + '.staticflickr.com/' + str(pick['server']) + '/' + str(pick['id']) + '_' + str(pick['secret']) + '_z.jpg'
+	except IndexError:
+		# there were no results, so the random.choice call failed above. This is OK, we'll just move on.
+		url = None
+
+	return url
+
+#given a list of sentences, return a list of paragraphs
 def graphize(sentences):
-	speech = ""
+	grafs = []
+	currentgraf = ""
 	counter = 0
 	target = initTarget()
 
 	for sentence in sentences:
-		speech += sentence
+		currentgraf += sentence
 		if counter == target:
 			if DEBUG: print("Ending a graf after " + str(target) + " lines.", file=sys.stderr)
-			speech += "\n\n"
+			grafs.append(currentgraf)
 			counter = 0
 			target = initTarget()
+			currentgraf = ""
 		else:
-			speech += " "
+			currentgraf += " "
 			counter += 1
 
-	speech = speech + "\n\nThank you.\n\n(Applause)"
+	grafs.append("Thank you.")
+	grafs.append("(Applause)")
 
-	return speech
+	return grafs
 
 
 #get a list of sentences to make into a speech, then call a utility to format it
@@ -75,9 +112,16 @@ def createTalk(seed):
 		sentences.append(newsentence)
 		if DEBUG: print(str(speechlength) + " - " + str(len(sentences)), file=sys.stderr)
 
-	speech = graphize(sentences)
+	grafs = graphize(sentences)
+	slides = []
+	for graf in grafs:
+		slides.append({"image": getImage(graf), "text": graf})
 
-	return speech
+	talk = {}
+	talk['title'] = "title goes here"
+	talk['slides'] = slides
+
+	return json.dumps(talk)
 
 def main():
 	if len(sys.argv) == 1:
