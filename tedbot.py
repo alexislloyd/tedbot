89e70634a43f42ac4a95cf6d865c868748948b2b
Matt Boggie <mboggie@mac.com>
2016-02-20 00:03:52 -0500

    adding graf parsing + DEBUG setting to simplify logging

--- tedbot.py
+++ tedbot.py
@@ -1,7 +1,41 @@
 from __future__ import print_function
 import markovify
 import sys
+import math
+import random
+import re
 
+GRAF_LENGTH = 8
+GRAF_RANGE = 3
+DEBUG = True
+
+# generate a target length for a paragraph, of the range LENGTH +/- [0-RANGE]
+def initTarget():
+	return GRAF_LENGTH + int(math.floor(random.triangular(0-GRAF_RANGE, GRAF_RANGE)))
+
+#given a list of sentences, create a long string broken up into paragraphs
+def graphize(sentences):
+	speech = ""
+	counter = 0
+	target = initTarget()
+
+	for sentence in sentences:
+		speech += sentence
+		if counter == target:
+			if DEBUG: print("Ending a graf after " + str(target) + " lines.", file=sys.stderr)
+			speech += "\n\n"
+			counter = 0
+			target = initTarget()
+		else:
+			speech += " "
+			counter += 1
+
+	speech = speech + "\n\nThank you.\n\n(Applause)"
+
+	return speech
+
+
+#get a list of sentences to make into a speech, then call a utility to format it
 def createTalk(seed):
 	with open("./transcripts.txt") as f:
 		text = f.read()
@@ -13,7 +47,7 @@ def createTalk(seed):
 	if seed:
 		beginning = seed
 		words = seed.split()
-		if len(words) > 2:
+		if len(words) >= 2:
 			end = len(words)
 			seed = words[end-2] + " " + words[end-1]
 		else:
@@ -30,20 +64,18 @@ def createTalk(seed):
 	if firstsentence is None:
 		print("Seed generated no results. Falling back.", file=sys.stderr)
 		firstsentence = markovmodel.make_sentence()
-		speech = firstsentence
 	else:
-		speech = beginning + " " + firstsentence
-	speechlength = len(speech.split())
-	sentences = 1
+		firstsentence = beginning + " " + firstsentence
+	speechlength = len(firstsentence.split())
+	sentences = [firstsentence]
 
 	while speechlength < 1800:
 		newsentence = markovmodel.make_sentence()
-		speech += " " + newsentence
 		speechlength += len(newsentence.split())
-		sentences +=1
-		print(str(speechlength) + " - " + str(sentences), file=sys.stderr)
+		sentences.append(newsentence)
+		if DEBUG: print(str(speechlength) + " - " + str(len(sentences)), file=sys.stderr)
 
-	speech = speech + "\n\nThank you.\n\n(Applause)"
+	speech = graphize(sentences)
 
 	return speech
 
